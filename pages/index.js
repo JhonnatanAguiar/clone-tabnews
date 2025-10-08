723e5c2e372826527503e0aa82cf4a7ea5868ac5
Jhonnatan Aguiar <81327493+JhonnatanAguiar@users.noreply.github.com>
2025-10-08 01:27:55 +0000

    Correção do palavrão

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return  <h1>Olá, Mundo! Esse é meu estudo do Curso.dev com Filipe Deschamps</h1>
+    return  <h1>Olá, Mundo! Esse é meu estudo do Curso.dev com Filipe Deschamps. Esse curso é muito fantástico!</h1>
 }
 
 export default Home
