072bed5b7d26012117c5ccb0b6b4e905f33635a7
Jhonnatan Aguiar <81327493+JhonnatanAguiar@users.noreply.github.com>
2025-10-15 14:06:18 +0000

    add `/api/v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Olá, Mundo!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("Indentação errada");
-}
-
 export default Home;
