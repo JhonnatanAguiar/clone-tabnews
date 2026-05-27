ce824632fe5077422af58ac6a275137faf0391dd
Jhonnatan Aguiar <81327493+JhonnatanAguiar@users.noreply.github.com>
2026-05-27 18:06:44 +0000

    refactor: change `DatabaseInfo` to `DatabaseStatus` and separate `UpdatedAt` from it

--- pages/status/index.js
+++ pages/status/index.js
@@ -11,37 +11,52 @@ export default function StatusPage() {
   return (
     <>
       <h1>Status</h1>
-      <DatabaseInfo />
+      <UpdatedAt />
+      <DatabaseStatus />
     </>
   );
 }
 
-function DatabaseInfo() {
+function UpdatedAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
   });
 
-  const updatedAtText = () => {
-    if (!isLoading && data) {
-      return new Date(data.updated_at).toLocaleString("pt-BR");
-    }
+  let updatedAt = "Carregando...";
 
-    return "Carregando...";
-  };
+  if (!isLoading && data) {
+    updatedAt = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Última atualização: {updatedAt}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
 
   const db = data?.dependencies?.database;
   const dbVersion = db?.version;
   const dbMaxConnections = db?.max_connections;
   const dbOpenedConnections = db?.opened_connections;
 
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <p>Versão: {dbVersion}</p>
+        <p>Conexões máximas: {dbMaxConnections}</p>
+        <p>Conexões abertas: {dbOpenedConnections}</p>
+      </>
+    );
+  }
+
   return (
     <>
-      <div>Última atualização {updatedAtText()}</div>
-
       <h3>Status do Banco de Dados</h3>
-      <p>Versão: {dbVersion}</p>
-      <p>Conexões máximas: {dbMaxConnections}</p>
-      <p>Conexões abertas: {dbOpenedConnections}</p>
+      <div>{databaseStatusInformation}</div>
     </>
   );
 }
